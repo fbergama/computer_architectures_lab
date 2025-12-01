b92a14dc7424507bad67ffc554f3073c519a41ea
Filippo Bergamasco <filippo.bergamasco@unive.it>
2025-12-01 22:23:26 +0100

    old comments removed

--- examples/02-arrays/arrays_v1.s
+++ examples/02-arrays/arrays_v1.s
@@ -46,54 +46,6 @@ exitfor:
     adr x4, r               // x4 = &r
     str x3, [x4]            // r = x3
 
-    /**/
-
-    // for loop, array operations using pointers
-    // Equivalent C code:
-    // 
-    //        x0 = (&A[0])
-    //        x1 = n
-    //        x2 = x0 + n*sizeof(int)  // pointer to the end of array
-    //        x3 = result
-    //        
-    //        for( x3=0; x0<x2; x0+=sizeof(int) )
-    //        {
-    //            if( mem[x0] == 0 )
-    //                x3+=1;
-    //        } 
-    // 
-    //  In this version it is not necessary to 
-    //  compute the address of ith element with
-    //  the instruction
-    //  add x4, x0, x2, LSL #2  //x4 = $(A[x2])
-    //  So, it requres less clock cycles
-    //
-    /*
-    adr x0, A               // Pointer to the first element of A
-    adr x1, n
-    ldr x1, [x1]
-
-    add x2, x0, x1, LSL #2  // Pointer to the end of A
-
-    mov x3, #0              // for initialization
-    cmp x0,x2
-    b.ge exitfor
-
-initfor:    
-    ldr w5, [x0]            //w5 = A[x0]
-    cmp w5, #0
-    b.ne forinc             // if(w5 != 0) goto for-inc
-    add x3,x3,#1            // x3=x3+1
-forinc:
-    add x0,x0,#4            // x0=x0+sizeof(int)
-    cmp x0,x2           
-    b.lt initfor            // if( x0<x2 ) goto init-for
-exitfor:
-    adr x4, r               // x4 = &r
-    str x3, [x4]            // r = x3
-    /**/
-
-
 
 exit:
     // Exit with return code 0
